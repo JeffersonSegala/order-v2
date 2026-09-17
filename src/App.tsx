754bedcd72825309155dc241d6743f825e700a3c
Jefferson Segala <beperson@gmail.com>
2026-09-17 13:26:14 -0300

    Add video component to App and update WatchList with new names

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './Components/Header';
 import OnlineMembers from './Components/OnlineMembers/OnlineMembers'
 import WatchList from './Components/WatchList/WatchList';
+import Window from './Components/Window/Window';
 import { OrderProvider } from './contexts/OrderContext';
 
 function App() {
@@ -16,6 +17,17 @@ function App() {
           </div>
           <div>
             <WatchList />
+          </div>
+        </div>
+
+        <div className='windowsContainer'>
+          <div style={{paddingTop: '2rem'}}>
+            <Window title="Nosso fã Nº1" isOpen={false}>
+              <video className="featuredVideo" controls preload="metadata">
+                <source src="/xolpicmin.mp4" type="video/mp4" />
+                Seu navegador não suporta a reprodução de vídeo.
+              </video>
+            </Window>
           </div>
         </div>
 
